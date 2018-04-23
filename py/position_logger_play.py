4b67a51d51346d2246ed8bfdea13f5babae82b38
Vincent Berenz <vincentberenz@gmail.com>
2018-04-23 17:34:36 +0200

    improvement of the layout of position_logger

--- py/position_logger_play.py
+++ py/position_logger_play.py
@@ -23,28 +23,36 @@
 import playful
 
 class position_logger(playful.Node):
+
     def execute(self):
+
         # value of POSITION_LOGGER_FREQUENCY is declared in ../config/globals.py
         frequency = playful.get_global("POSITION_LOGGER_FREQUENCY")
+
         while not self.should_pause():
+
             # returns a dict key: scheme_id, value: position
             # this dict will contain information on all schemes
             # having a position property
             positions = playful.memory.get_property_value("position")
+
             # returns a dict key: scheme_type (e.g. ball), value: list of scheme_ids of this type
             type_id = playful.memory.get_scheme_type_scheme_id()
+
             # function to get the type of a scheme based on the scheme_id
             def get_type(type_id,scheme_id):
                 for type in type_id.keys():
                     ids = type_id[type]
                     if scheme_id in ids : return type
                 return None
+
             # print in console list: scheme type: position
             r = ["\nPOSITIONS:"]
             for scheme_id,position in positions.iteritems():
                 type = get_type(type_id,scheme_id)
                 if type: r.append("\t"+type+": "+str(position))
             playful.console(str(id(self)),"\n".join(r)+"\n")
+
             # running at configured frequency
             self.spin(frequency)
             
